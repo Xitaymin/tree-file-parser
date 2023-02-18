b1ab08255148e105a779767cb4ec94cfd8f49d70
Yaroslav Barashenko <y.barashenko@gmail.com>
2023-02-18 19:02:49 +0200

    Made refactoring and added tests

--- src/main/kotlin/Directory.kt
+++ src/main/kotlin/Directory.kt
@@ -1,13 +1,8 @@
 import java.util.*
 
-data class Directory(val directoryName: String, val directorySize: Long, val parent: Directory?, val fullPath: String) : Comparable<Directory> {
+data class Directory( val directorySize: Long, val fullPath: String) {
     val files = ArrayList<TreeFile>()
-    val directories = LinkedList<Directory>() //todo make it be tree
-    var totalSize = 0L
-    override fun compareTo(other: Directory): Int = this.fullPath.compareTo(other.fullPath)
-    override fun toString(): String {
-        return """$directoryName|$directorySize files count = ${files.size} directories count = ${directories.size} full path = $fullPath
-            |directories = ${directories.map { it.fullPath }}
-        """.trimMargin()
-    }
+    val directories = LinkedList<Directory>()
+
+
 }
